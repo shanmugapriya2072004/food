b397297af80f76cc61e578ccf7c2ae5e7089a828
shanmugapriya <shanmugapriya2072004@gmail.com>
2026-04-01 16:04:58 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+import Home from "./Home";
+import About from "./About";
+import Work from "./Work";
+import Testimonial from "./Testimonial";
+import Contact from "./Contact";
+import Footer from "./Footer";
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+      <About />
+      <Work />
+      <Testimonial />
+      <Contact />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
